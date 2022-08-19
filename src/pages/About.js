cfe76545a3098d4602fb1e64b1e09bcec8f4a438
Mael <contact@maeldev.fr>
2022-08-19 18:27:41 +0200

    first Version

---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus alias
+        soluta rem qui iure quasi similique veniam animi nihil dolorum provident
+        nobis quo dicta officiis, voluptate placeat corrupti error fugit? Fugiat
+        molestias, recusandae consectetur saepe expedita maxime neque excepturi
+        nulla doloremque dolore eius impedit temporibus repellendus quia aperiam
+        ratione nemo consequatur ullam vel necessitatibus maiores ducimus a.
+        Officia reprehenderit recusandae ab corrupti sint quo maxime rem maiores
+        natus aperiam id hic doloremque, incidunt quos minus repellendus
+        possimus exercitationem consectetur nesciunt iste libero, in vel sit
+        accusamus? Nam, quidem a! Harum, quam? Nobis ullam voluptas nam tenetur
+        at dolorem ad voluptate.
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet quo
+        quisquam cupiditate perspiciatis saepe? Similique deserunt, at placeat
+        unde doloribus vitae eius odit inventore ratione fuga, qui recusandae
+        assumenda quam explicabo soluta, quibusdam atque. Nam omnis id
+        perferendis excepturi architecto modi. Beatae mollitia rerum similique
+        doloremque libero porro nam. Nobis at aliquam aliquid ullam modi
+        blanditiis, quo similique iste itaque autem, perspiciatis doloremque
+        eaque maiores cumque libero consectetur animi dolor provident illo, sint
+        excepturi? Officia dolore exercitationem aut amet sit, odio ab
+        voluptates maiores, cumque nesciunt iure, ipsa soluta temporibus quia
+        doloremque earum deleniti animi doloribus fuga voluptatibus omnis
+        debitis minus. Alias soluta aut quis temporibus atque beatae voluptatem,
+        vel quia saepe obcaecati nesciunt quidem, rem quae delectus tempora
+        exercitationem accusamus architecto minima odit debitis facere deleniti!
+        Dignissimos voluptas eius at exercitationem possimus ex quae rerum
+        excepturi, obcaecati unde commodi non architecto illo. Voluptatem id
+        blanditiis magnam? Ut ex quo nesciunt officia eaque aliquam sunt
+        accusamus, error iure illum exercitationem unde voluptatum soluta quae
+        cum praesentium earum eius, non saepe voluptates voluptas! Doloremque
+        ratione ad dolor. Quibusdam ducimus, consectetur commodi dolorem
+        necessitatibus consequatur eveniet ipsam expedita molestiae pariatur
+        cupiditate obcaecati, ad libero atque veniam quae quis laboriosam eos ex
+        temporibus?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const Error = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>Error 404 ! Page not found.</h1>
+    </div>
+  );
+};
+
+export default Error;
